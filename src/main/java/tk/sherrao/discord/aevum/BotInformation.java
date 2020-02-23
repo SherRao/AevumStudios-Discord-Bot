c4edee30f9c26b35e53287bd2fb97625fb66aeab
SherRao <SherRao15@gmail.com>
2020-02-23 15:28:26 -0500

    Updated BotInformation

---
+++ src/main/java/tk/sherrao/discord/aevum/BotInformation.java
@@ -0,0 +1,49 @@
+package tk.sherrao.discord.aevum;
+
+public class BotInformation {
+
+    
+    public static final String DISCORD_TOKEN = null;
+
+    public static final String TICKETS_CATEGORY_ID = null;
+    
+    public static final String COMMANDS_CHANNEL_ID = null;
+    
+    public static final String COMMISSIONS_TICKET_CHANNEL_ID = null;
+    
+    public static final String MEMBER_ROLE_ID = null;
+    
+    public static final String WARNING_UNICODE = "⚠️";
+    
+    public static final String SUCCESS_UNICODE =  "✅";
+    
+    public static final String ERROR_UNICODE = "⛔";
+    
+    
+
+    public static final String DEVELOPMENT_ROLE_ID = null;
+    
+    public static final String DESIGN_ROLE_ID = null;
+    
+    public static final String BUILD_ROLE_ID = null;
+    
+    
+    /** INVALID */
+    public static final String WEBDEV_DEPARTMENT_ROLE_ID = null;
+    
+    public static final String MCDEV_DEPARTMENT_ROLE_ID = null;
+    
+    public static final String DISCORDDEV_DEPARTMENT_ROLE_ID = null;
+
+    public static final String GRAPHICS_DEPARTMENT_ROLE_ID = null;
+
+    public static final String BUILDING_DEPARTMENT_ROLE_ID = null;
+
+
+    public static final String SHERRAO_ID = null;
+    
+    public static final String SERVER_ID = null;
+    
+    public static final String INVITE = null;
+    
+}
